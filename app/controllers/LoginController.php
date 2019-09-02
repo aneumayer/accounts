5b4c0df8a82e60156dfeff42057001d380b7b145
Anthony Neumayer <aneumayer@gmail.com>
2019-09-02 14:32:23 -0400

    Removed custom message in favor of flash

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -9,25 +9,27 @@ class LoginController extends \Phalcon\Mvc\Controller
             // Get the values from the form
             $username = strtolower($this->request->getPost("username"));
             $password = $this->request->getPost("password");
+           
             // See if a user with that username exists
             $user = User::findFirst([
                 'conditions' => 'username = :username:',
                 'bind'       => ['username'=> $username]
             ]);
+            
             // Verify if the password is a match for the hash in the database
             if ($user == false) {
-                $this->view->alert = [
-                    "message" => "Username and password do not match.",
-                    "type"    => "danger"
-                ];
+                $this->flash->error("Username and password do not match.");
+
             } else if ($this->security->checkToken() && $this->security->checkHash($password, $user->password)) {
                 // The user is authenticated send them to the homepage
-                $this->response->redirect('index');
+                $fullname = $user->first_name . " " . $user->last_name;
+                $this->session->set('fullname', $fullname);
+                $this->session->set('user', $user);
+                $this->flash->success("Welcome back " . $fullname);
+                return $this->dispatcher->forward(["controller" => "index", "action" => "index"]);
+
             } else {
-                $this->view->alert = [
-                    "message" => "An error occured while attempting to log in.",
-                    "type"    => "danger"
-                ];
+                $this->flash->error("An error occured while attempting to log in.");
             }
         }
     }
--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -11,21 +11,19 @@ class SignupController extends \Phalcon\Mvc\Controller
             $last_name  = $this->request->getPost('last_name');
             $username   = strtolower($this->request->getPost('username'));
             $password   = $this->security->hash($this->request->getPost('password'));
+            
             // Check if the username is already in use
             $user = User::findFirst([
                 'conditions' => 'username = :username:',
                 'bind'       => ['username'=> $username]
             ]);
+            
             if ($user) {
-                $this->view->alert = [
-                    "message" => "Username is already in use.",
-                    "type"    => "warning"
-                ];
+                $this->flash->error("Username is already in use.");
+            
             } else if ($this->request->getPost('password') != $this->request->getPost('password2')) {
-                $this->view->alert = [
-                    "message" => "Password fields do not match.",
-                    "type"    => "warning"
-                ];
+                $this->flash->error("Password fields do not match.");
+            
             } else if ($this->security->checkToken()) {
                 // Create a new user with the values from the form
                 $user = new User();
@@ -34,15 +32,15 @@ class SignupController extends \Phalcon\Mvc\Controller
                 $user->username    = $username;
                 $user->password    = $password;
                 $user->date_created = new \Phalcon\Db\RawValue("now()");
+                
                 // Save the new user to the database
                 if ($user->save() === false) {
-                    $this->view->alert = [
-                        "message" => "Error signing up.",
-                        "type"    => "danger"
-                    ];
+                    $this->flash->error("Error signing up.");
+                
                 } else {
                     // The user is authenticated send them to the homepage
-                    $this->response->redirect('index');
+                    $this->flash->success("Welcome " . $fullname);
+                    return $this->dispatcher->forward(["controller" => "index", "action" => "index"]);
                 }
             }
             
