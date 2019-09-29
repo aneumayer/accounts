c6f3c42a0cd52e72520ce43a7120a2c22e4fe826
Anthony Neumayer <aneumayer@gmail.com>
2019-09-28 21:29:26 -0400

    Created pages for renaming and deleting folders

--- app/config/router.php
+++ app/config/router.php
@@ -4,4 +4,21 @@ $router = $di->getRouter();
 
 // Define your routes here
 
+// Rename an existing folder
+$router->add(
+    '/folder/rename/{id}',
+    [
+        "controller" => "folder",
+        "action"     => "rename",
+    ]
+)->setName("folder-rename");
+// Delete an existing folder
+$router->add(
+    '/folder/delete/{id}',
+    [
+        "controller" => "folder",
+        "action"     => "delete",
+    ]
+)->setName("folder-delete");
+
 $router->handle();
--- app/controllers/FolderController.php
+++ app/controllers/FolderController.php
@@ -12,11 +12,16 @@ class FolderController extends \Phalcon\Mvc\Controller
         }
     }
 
+    /**
+     * Add a new folder for the logged in user
+     *
+     * @return void
+     */
     public function addAction()
     {
         if ($this->request->isPost()) {
             $folder_name = $this->request->getPost('folder_name');
-            
+
             // See if a folder with that name already exists
             $folder = Folder::findFirst([
                 'conditions' => 'user_id = :user_id: AND name = :folder_name:',
@@ -25,7 +30,7 @@ class FolderController extends \Phalcon\Mvc\Controller
                     'folder_name' => $folder_name
                 ]
             ]);
-        
+
             // If there is no folder create it
             if ($folder === false && $this->security->checkToken()) {
                 $folder = new Folder();
@@ -34,10 +39,67 @@ class FolderController extends \Phalcon\Mvc\Controller
                 $folder->date    = new \Phalcon\Db\RawValue("now()");
                 $folder->save();
                 return $this->response->redirect('');
-
             } else {
                 $this->flash->error("A folder with this name already exists.");
             }
+        }
+    }
+
+    /**
+     * Form to rename the given folder
+     *
+     * @return void
+     */
+    public function renameAction()
+    {
+        // Get the folder being referenced if it belongs to the user
+        $folder_id = $this->dispatcher->getParam("id");
+        $folder = Folder::findFirst([
+            'conditions' => 'user_id = :user_id: AND id = :folder_id:',
+            'bind'       => [
+                'user_id'     => $this->user->id,
+                'folder_id' => $folder_id
+            ]
+        ]);
+
+        // If the folder exists and the form has been posted rename the folder
+        if ($this->request->isPost() && $this->security->checkToken()) {
+            $folder->name = $this->request->getPost('folder_name');
+            $folder->save();
+            return $this->response->redirect('');
+        } elseif ($folder !== false) {
+            $this->view->folder_name = $folder->name;
+        } else {
+            $this->flash->error("No folder was found");
+            return $this->response->redirect('');
+        }
+    }
+
+    /**
+     * Form to delete the given folder
+     *
+     * @return void
+     */
+    public function deleteAction()
+    {
+        // Get the folder being referenced if it belongs to the user
+        $folder_id = $this->dispatcher->getParam("id");
+        $folder = Folder::findFirst([
+            'conditions' => 'user_id = :user_id: AND id = :folder_id:',
+            'bind'       => [
+                'user_id'     => $this->user->id,
+                'folder_id' => $folder_id
+            ]
+        ]);
+
+        if ($this->request->isPost() && $this->security->checkToken()) {
+            $folder->delete();
+            return $this->response->redirect('');
+        } elseif ($folder !== false) {
+            $this->view->folder_name = $folder->name;
+        } else {
+            $this->flash->error("No folder was found");
+            return $this->response->redirect('');
         }
     }
 
